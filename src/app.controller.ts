37a68d96ad3d6c5113cc27c38f6f202acc073fc7
X932 <yusuf_020202@mail.ru>
2022-04-09 10:45:59 +0500

    implemented jwt strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { JwtAuthGuard } from '@guards/jwt-auth.guard';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor() {}
 
+  @UseGuards(JwtAuthGuard)
   @Get('t')
   getTest() {
     return 'works';
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { ClientsModule } from '@nestjs/microservices';
 import { universityServiceConfig } from '@configs/services';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   controllers: [AuthController],
   imports: [ClientsModule.register([universityServiceConfig])],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
